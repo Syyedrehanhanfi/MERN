3418bae6ab26cf6d8fb92c5f663cbfa9480290fa
syyedrehanhanfi <syyedrehanhanfi33@gmail.com>
2026-05-09 17:47:31 +0530

    connect to mongoDB atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("hello")
+import connectDB from './db/index.js';
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: './.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express';
+
+const app =express()
+
+;(
+  async()=>{
+      try {
+        const connect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("Error",(error)=>{
+          console.log("Error :", error);
+          throw error
+          
+        })
+
+        app.listen(process.env.PORT, ()=>{
+          console.log(`App is listening on port ${process.env.PORT}`);
+          
+        })
+
+      } catch (error) {
+        console.error("Error :", error),
+        throw error
+      }
+  }
+)()
+  */
